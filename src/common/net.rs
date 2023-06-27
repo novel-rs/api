7f86d4696282a14e3d868d88af2f81691bb5cf6d
Terakomari <im.terakomari@icloud.com>
2023-06-27 16:43:21 +0800

    chore(net): bump user agent

--- src/common/net.rs
+++ src/common/net.rs
@@ -57,7 +57,7 @@ impl HTTPClientBuilder {
                 "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
             ),
             accept_language: HeaderValue::from_static("zh-CN,zh;q=0.9"),
-            user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36".to_string(),
+            user_agent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36".to_string(),
             cookie: false,
             allow_compress: true,
             proxy: None,
