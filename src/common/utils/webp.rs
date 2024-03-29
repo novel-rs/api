fc0e0365258664fa2a6f85b188c4a16a65232af1
Terakomari <im.terakomari@icloud.com>
2024-03-29 14:37:11 +0800

    refactor(utils): change save_as_webp's parameter

--- src/common/utils/webp.rs
+++ src/common/utils/webp.rs
@@ -5,23 +5,19 @@ use webp::Encoder;
 
 use crate::Error;
 
-pub fn save_as_webp<T>(image: DynamicImage, quality: f32, path: T) -> Result<(), Error>
+pub fn save_as_webp<T>(image: &DynamicImage, quality: f32, path: T) -> Result<(), Error>
 where
     T: AsRef<Path>,
 {
-    let encoder = match &image {
+    let encoder = match image {
         DynamicImage::ImageLuma8(_) => Err(String::from("Unimplemented")),
         DynamicImage::ImageLumaA8(_) => Err(String::from("Unimplemented")),
-        DynamicImage::ImageRgb8(image) => Ok(Encoder::from_rgb(
-            image.as_ref(),
-            image.width(),
-            image.height(),
-        )),
-        DynamicImage::ImageRgba8(image) => Ok(Encoder::from_rgba(
-            image.as_ref(),
-            image.width(),
-            image.height(),
-        )),
+        DynamicImage::ImageRgb8(image) => {
+            Ok(Encoder::from_rgb(image, image.width(), image.height()))
+        }
+        DynamicImage::ImageRgba8(image) => {
+            Ok(Encoder::from_rgba(image, image.width(), image.height()))
+        }
         _ => Err(String::from("Unimplemented")),
     }
     .map_err(Error::Webp)?;
