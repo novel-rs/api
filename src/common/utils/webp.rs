e255ddffe1db45f382dc5adc9f60de3ca5a787e7
Terakomari <im.terakomari@icloud.com>
2024-04-22 10:06:29 +0800

    refactor: simplify webp saving

--- src/common/utils/webp.rs
+++ src/common/utils/webp.rs
@@ -9,19 +9,7 @@ pub fn save_as_webp<T>(image: &DynamicImage, quality: f32, path: T) -> Result<()
 where
     T: AsRef<Path>,
 {
-    let encoder = match image {
-        DynamicImage::ImageLuma8(_) => Err(String::from("Unimplemented")),
-        DynamicImage::ImageLumaA8(_) => Err(String::from("Unimplemented")),
-        DynamicImage::ImageRgb8(image) => {
-            Ok(Encoder::from_rgb(image, image.width(), image.height()))
-        }
-        DynamicImage::ImageRgba8(image) => {
-            Ok(Encoder::from_rgba(image, image.width(), image.height()))
-        }
-        _ => Err(String::from("Unimplemented")),
-    }
-    .map_err(Error::Webp)?;
-
+    let encoder = Encoder::from_image(image).map_err(|err| Error::Webp(err.to_string()))?;
     let webp = encoder.encode(quality);
     fs::write(path, &*webp)?;
     Ok(())
